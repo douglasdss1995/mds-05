97868fc8d63c3e4fb0702c52879c14b063818b36
douglas.silva <douglas.silva@fpf.br>
2025-09-03 10:26:29 -0400

    Varaiveis

---
+++ javascript/variaveis/variaveis.js
@@ -0,0 +1,36 @@
+var vVar = 10;
+let vLet = 20;
+const V_CONST = 30;
+
+var indefinido;
+console.log(indefinido);
+
+var numero = 1;
+console.log(numero);
+
+var numeroGrande = 1234567890123456789012345678901234567890n;
+console.log(numeroGrande);
+
+var nome = "Maria";
+console.log(nome);
+
+var ativo = true;
+console.log(ativo);
+var inativo = false;
+console.log(inativo);
+
+// Objeto
+var objeto = {
+  nome: "João",
+  sobrenome: "Silva",
+};
+console.log(objeto);
+
+
+//Array/List
+var array = [1, 2, 3, 4, 5];
+console.log(array);
+
+//Data
+let data = new Date();
+console.log(data);
