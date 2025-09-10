f3954b7dca23b34075cc607c7fddee6ed6489613
douglas.silva <douglas.silva@fpf.br>
2025-09-10 11:34:34 -0400

    Angular - Data Binding

---
+++ angular/hello-world/src/app/data-binding/interpolation/interpolation.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  imports: [],
+  standalone: true,
+  templateUrl: './interpolation.component.html',
+  styleUrl: './interpolation.component.css'
+})
+export class InterpolationComponent {
+
+  ola: string = "Olá alunos!";
+
+}
