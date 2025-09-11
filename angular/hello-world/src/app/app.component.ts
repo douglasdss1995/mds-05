3524f593361ada11edbf9dcffb278dfa8bbad398
douglas.silva <douglas.silva@fpf.br>
2025-09-11 08:55:45 -0400

    Angular - Routers

--- angular/hello-world/src/app/app.component.ts
+++ angular/hello-world/src/app/app.component.ts
@@ -1,18 +1,10 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { EventBindingComponent } from './data-binding/event-binding/event-binding.component';
-import { InterpolationComponent } from './data-binding/interpolation/interpolation.component';
-import { PropertyBindingComponent } from './data-binding/property-binding/property-binding.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
+import {Component} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    HelloWorldComponent,
-    InterpolationComponent,
-    PropertyBindingComponent,
-    EventBindingComponent
+    RouterModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- angular/hello-world/src/app/app.routes.ts
+++ angular/hello-world/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {TemplateDrivenComponent} from './forms/template-driven/template-driven.component';
+import {TwoWayDataBindingComponent} from './data-binding/two-way-data-binding/two-way-data-binding.component';
+import {InterpolationComponent} from './data-binding/interpolation/interpolation.component';
+import {EventBindingComponent} from './data-binding/event-binding/event-binding.component';
+import {PropertyBindingComponent} from './data-binding/property-binding/property-binding.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "template-drive", component: TemplateDrivenComponent},
+  {path: "two-way-data-binding", component: TwoWayDataBindingComponent},
+  {path: "interpolation", component: InterpolationComponent},
+  {path: "event-binding", component: EventBindingComponent},
+  {path: "property-binding", component: PropertyBindingComponent},
+];
