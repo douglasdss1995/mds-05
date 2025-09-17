19b8a9894b3be7d8c579e212f2a357f5cba02465
douglas.silva <douglas.silva@fpf.br>
2025-09-17 10:54:14 -0400

    Android - Add validações e teclado

--- android/IMC/app/src/main/java/com/cursodedesenvolvimentodesoftware/imc/ui/views/ImcCalculator.kt
+++ android/IMC/app/src/main/java/com/cursodedesenvolvimentodesoftware/imc/ui/views/ImcCalculator.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -17,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cursodedesenvolvimentodesoftware.imc.ui.theme.IMCTheme
@@ -42,29 +44,42 @@ fun ImcCalculator(
         var pesoInput by remember { mutableStateOf("") }
         // Estado para armazenar o valor do campo de texto da altura.
         var alturaInput by remember { mutableStateOf("") }
-        // Estado para armazenar o resultado do cálculo do IMC (pode ser nulo se a entrada for inválida).
+        // Estado para armazenar o resultado do cálculo do IMC
+        // (pode ser nulo se a entrada for inválida).
         var imcResult by remember { mutableStateOf<Double?>(null) }
-        // Estado para armazenar o texto da classificação do IMC ou mensagens de erro.
+        // Estado para armazenar o texto da classificação do
+        // IMC ou mensagens de erro.
         var resultadoTexto by remember { mutableStateOf("") }
         // Gerenciador de foco para controlar o teclado virtual.
         val focusManager = LocalFocusManager.current
 
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = pesoInput,
+            onValueChange = {
+                pesoInput = it.filter { char ->
+                    char.isDigit() || char in setOf(
+                        '.',
+                        ','
+                    )
+                }
+            },
             label = {
                 Text(text = "Peso")
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         )
 
         OutlinedTextField(
-            value = "",
-            onValueChange = {},
+            value = alturaInput,
+            onValueChange = {
+                alturaInput = it.filter { char -> char.isDigit() || char == '.' }
+            },
             label = {
                 Text(text = "Altura")
             },
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
         )
 
         Spacer(modifier = Modifier.height(10.dp))
@@ -75,6 +90,12 @@ fun ImcCalculator(
         ) {
             Text(text = "Calcular")
         }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = imcResult.toString()
+        )
 
     }
 }
